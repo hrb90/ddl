d02de43b44c3a4662973924d6202cf5beb881155
hrb90 <harrisonrbrown90@gmail.com>
2017-03-03 11:34:36 -0500

    Refactor appender logic into subdirectory

---
+++ scripts/appenders/color_pickers.js
@@ -0,0 +1,16 @@
+var positionPicker = function(player) {
+  switch(player.position) {
+    case "PG":
+      return "red";
+    case "SG":
+      return "orange";
+    case "SF":
+      return "yellow";
+    case "PF":
+      return "green";
+    case "C":
+      return "blue";
+  }
+};
+
+module.exports = { positionPicker }
