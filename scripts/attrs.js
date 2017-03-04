3098ee75700306bf01eaa5407b730b53b3997e71
hrb90 <harrisonrbrown90@gmail.com>
2017-03-04 17:23:23 -0500

    Add options to selectors in JS

---
+++ scripts/attrs.js
@@ -0,0 +1,23 @@
+var basicAttributes = {
+  tsPct: "TS%",
+  astPct: "AST%",
+  oRbPct: "ORB%",
+  tRbPct: "TRB%",
+  stlPct: "STL%",
+  blkPct: "BLK%",
+  usgPct: "USG%",
+  tovPct: "TOV%",
+  obpm: "OBPM",
+  dbpm: "DBPM",
+  bpm: "BPM",
+  minutes: "Minutes"
+};
+
+var filterAttributes = {
+  age: "Age"
+};
+
+module.exports = {
+  basicAttributes: basicAttributes,
+  filterAttributes: filterAttributes 
+};
