0d3eaf9a4828ee17e264518795d5743f4ea62ffe
hrb90 <harrisonrbrown90@gmail.com>
2017-04-13 02:40:44 -0400

    Rebundle with placeholder data

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ var TooltipFactories = require('./appenders/tooltip_factory');
 var Gatherer = require('./gatherer');
 var makeFilterSpan = require('./make_filter_span');
 var attributes = require('./attrs');
-var nbaData = require('../data/all_data.json');
+var nbaData = require('../data/placeholder_data.json');
 
 
 function deserializeView(viewObject) {
